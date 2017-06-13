3840cf152bb58614ea2067ea433ed686b9569bb6
jovton <johan.vantonder@sanlam.co.za>
2017-06-13 09:45:36 +0200

    basic example from Exam Ref 70-480 Microsoft Preby Rick Delorme (not working)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # javascript-drag-and-drop
-Demonstration of dragging and dropping all kinds of thingsand transfer data and objects around and displaying them in interesting ways once they get dropped in a new place.
+Demonstration of dragging and dropping all kinds of things and transfer data and objects around and displaying them in interesting ways once they get dropped in a new place.
---
+++ js/javascript-drag-and-drop.js
@@ -0,0 +1,27 @@
+(function (window, jovton, $, undefined) {
+    var chip = document.getElementById("chip");
+
+    chip.addEventListener("dragstart", function (e) {
+        e.dataTransfer.setData("Text", this.id);
+    });
+
+    var b1 = document.getElementById("bucket1");
+    b1.addEventListener("dragenter", function(e) {
+        b1.classList.add("over");
+        e.returnValue = false;
+    });
+    b1.addEventListener("dragleave", function() {
+        b1.classList.remove("over");
+    });
+    b1.addEventListener("dragover", function (e) {
+        e.returnValue = false;
+    });
+    b1.addEventListener("drop", function(e) {
+        e.returnValue = false;
+        var data = event.dataTransfer.getData("Text");
+        var d = document.getElementById(data);
+        d.classList.remove("begin");
+        d.classList.add("dropped");
+        this.appendChild(d);
+    });
+})(window, window.jovton = window.jovton || {});
